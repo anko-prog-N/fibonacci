f5f60c0c63862936c6544d1ce2d6099ae3c166af
anko-prog-n <akon8321@yahoo.co.jp>
2020-09-08 20:00:49 +0900

    fibonacci

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+const seqNums = new Map();
+seqNums.set(0, 0);
+seqNums.set(1, 1);
+const length = process.argv[2] || 0;
+function fibonacci(n) { 
+    if (!seqNums.has(n)) {
+        seqNums.set(n, fibonacci(n - 1) + fibonacci(n - 2));
+    }
+    return seqNums.get(n);
+}
+for (let i = 0; i < length; i++) {
+    console.log(fibonacci(i));
+}
